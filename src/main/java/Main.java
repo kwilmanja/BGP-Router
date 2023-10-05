cd7303d82469634c5713e241e623c82ad14175e9
kwilmanja <kwilman.j@northeastern.edu>
2023-10-05 16:57:11 -0400

    Parse command line args

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import org.json.JSONException;
+
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class Main {
@@ -5,7 +9,7 @@ public class Main {
 
 
 
-  public static void main(String[] args){
+  public static void main(String[] args) throws SocketException, UnknownHostException, JSONException {
     System.out.println("Hello Andrew Panzone");
     int asn = Integer.parseInt(args[0]);
 
@@ -13,6 +17,13 @@ public class Main {
     for(int i=1; i<args.length; i++){
       routers.add(new Router(args[i]));
     }
+
+
+    while (true) {
+
+    }
+
+
 
 
 
--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,15 +1,48 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Router {
 
   public RouterType type;
-  public Socket sock;
+  public DatagramSocket sock;
+  public String ip;
 
-  public Router(String routerString){
-    this.type = RouterType.of(routerString.substring(routerString.length() - 4));
-    int port = Integer.parseInt(routerString.substring(0, 4));
-    String ip = routerString.substring(5, routerString.length() - 5);
-    System.out.println(type + " " + port + " " + ip);
+
+  public Router(String routerString) throws SocketException, UnknownHostException, JSONException {
+
+    String[] strs = routerString.split("-");
+
+    this.type = RouterType.of(strs[2]);
+    this.ip = strs[1];
+
+    int port = Integer.parseInt(strs[0]);
+    this.sock = new DatagramSocket();
+    this.sock.connect(InetAddress.getByName("localhost"), port);
+    this.sendHandshake();
+  }
+
+  private void sendHandshake() throws JSONException {
+    JSONObject handshake = new JSONObject();
+    handshake.put("src", this.getOurIP());
+    handshake.put("dst", this.ip);
+    handshake.put("type", "handshake");
+    handshake.put("msg", new JSONObject());
+
+    handshake.toString().getBytes()
+
+  }
+
+
+
+  public String getOurIP(){
+    return ip.substring(0, ip.length()-1) + "1";
   }
 
 
